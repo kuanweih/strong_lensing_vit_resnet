b017faca0ef02310a1845ab7bc33f8234366a152
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2022-04-21 23:00:26 -0400

    functionalize data loaders

--- model_vit.py
+++ model_vit.py
@@ -4,49 +4,30 @@
 """
 
 
-import gc
 import os
-import sys
-import h5py
-import time
-import zipfile
 import datetime
-
-import numpy as np # linear algebra
-import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
-import seaborn as sns
-import matplotlib.pyplot as plt
-
-import scipy as sp
 import scipy.ndimage
-from scipy.ndimage import gaussian_filter, rotate
 
-from PIL import Image
+import numpy as np
+import pandas as pd
+
 from tqdm import tqdm
 from pathlib import Path
-from copy import deepcopy
 from tensorboardX import SummaryWriter
 
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 import torch.optim as optim
-import torchvision.models as models
 
-from torch.optim import lr_scheduler
-from torchvision import datasets, transforms, models, utils
+from torchvision import transforms
 from torch.autograd import Variable
 from torch.utils.data import Dataset, DataLoader
 
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score, recall_score, f1_score
-
-from transformers import ViTModel, ViTConfig, ViTFeatureExtractor, ViTForImageClassification
-
-
+from transformers import ViTForImageClassification
 
 
 class DeepLenstronomyDataset(Dataset):  # torch.utils.data.Dataset
+    
     def __init__(self, root_dir, train=True, transform=None, target_transform=None):
         self.root_dir = root_dir
         self.transform = transform
@@ -60,16 +41,14 @@ class DeepLenstronomyDataset(Dataset):  # torch.utils.data.Dataset
         if self.train:
             self.path = os.path.join(self.root_dir, self.train_folder)
             self.df = pd.read_csv(self.path + '/metadata.csv')  #TODO: right csv?
-            #self.length = TRAINING_SAMPLES
         else:
             self.path = os.path.join(self.root_dir, self.test_folder)
             self.df = pd.read_csv(self.path + '/metadata.csv')  #TODO: right csv?
-            #self.length = TESTING_SAMPLES
 
     def __getitem__(self, index):
         img_path = self.path + self.df['img_path'].values[index][-13:]
         img = np.load(img_path)
-        img = scipy.ndimage.zoom(img, 224/100, order=1)  #TODO: this is hard coded
+        img = scipy.ndimage.zoom(img, 224 / 100, order=1)  #TODO: this is hard coded
         image = np.zeros((3, 224, 224))  #TODO: this is hard coded
         for i in range(3):
             image[i, :, :] += img
@@ -102,8 +81,6 @@ class DeepLenstronomyDataset(Dataset):  # torch.utils.data.Dataset
         return self.df.shape[0]
 
 
-
-
 def print_n_train_params(model):
     n = sum(param.numel() for param in model.parameters() if param.requires_grad)
     print(f"Number of trainable parameters: {n}")
@@ -117,48 +94,49 @@ def prepare_data_and_target(data, target_dict):
     return data, target
 
 
-def calc_avg_rms(total_rms, total_counter):
-    avg_rms = total_rms / total_counter
+def calc_avg_rms(cache):
+    avg_rms = cache['total_rms'] / cache['total_counter']
     avg_rms = avg_rms.cpu()
     avg_rms = (avg_rms.data).numpy()
-    return avg_rms
-
-
-def add_scalar_tb(avg_rms):
+    cache['avg_rms'] = avg_rms
     for i in range(len(avg_rms)):
         tb.add_scalar(f"rms {i + 1}", avg_rms[i])
-
-
-
-
-if __name__ == '__main__':
-
-    EPOCH = 2
-    glo_batch_size = 16
-    test_num_batch = 50
-
-    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-    print(device)
-
-    # file name
-    model_name = "gz2_hug_vit_010822B"
-
-    # Vision Transformer
-    feature_extractor = ViTFeatureExtractor.from_pretrained('google/vit-base-patch16-224')
-    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
-    print_n_train_params(model)
-
-    # # freeze the weights
-    # for param in model.parameters():
-    #     param.requires_grad = False
-
-    # change the last layer
-    model.classifier = nn.Linear(in_features=768, out_features=18, bias=True)
-    print_n_train_params(model)
-
+    return cache
     
-    # LensDatasets
-    dataset_folder = Path("C:/Users/abcd2/Downloads/dev_256/")
+
+def print_loss_rms(epoch, train_test_str, cache):
+    loss_per_batch = cache['total_loss'] / cache['total_counter']
+    avg_rms_for_print = np.array_str(cache['avg_rms'], precision=4)
+    print(f"epoch = {epoch}, {train_test_str}:")
+    print(f"    loss (average per batch wise) = {loss_per_batch:.4e}")
+    print(f"    RMS (average per batch wise) = {avg_rms_for_print}")
+
+
+def update_cache(cache, pred, target, loss):
+    square_diff = pred - target  
+    cache['total_rms'] += square_diff.std(dim=0)
+    cache['total_loss'] += loss.item()
+    cache['total_counter'] += 1
+    return cache
+
+
+def initialize_cache():
+    cache = {
+        'total_loss': 0.0,
+        'total_counter': 0,
+        'total_rms': 0,
+    }
+    return cache
+
+
+def calc_loss(loss_fn, pred, target):
+    loss_theta_E = loss_fn(100*pred[0], 100*target[0])  #TODO: this is hard coded
+    loss_others = loss_fn(pred, target)
+    loss = loss_theta_E + loss_others
+    return loss
+
+
+def get_train_test_dataloaders(dataset_folder, batch_size):
 
     data_transform = transforms.Compose([
         transforms.ToTensor(), # scale to [0,1] and convert to tensor
@@ -172,9 +150,9 @@ if __name__ == '__main__':
         transform=data_transform, 
         target_transform=target_transform,
     )
-    train_loader = torch.utils.data.DataLoader(
+    train_loader = DataLoader(
         train_dataset,
-        batch_size=glo_batch_size, 
+        batch_size=batch_size, 
         shuffle=True,
     )
 
@@ -184,100 +162,102 @@ if __name__ == '__main__':
         transform=data_transform, 
         target_transform=target_transform,
     )
-    test_loader = torch.utils.data.DataLoader(
+    test_loader = DataLoader(
         test_dataset,
-        batch_size=glo_batch_size, 
-        shuffle=True,  #TODO shuffle for test set?
+        batch_size=batch_size, 
+        shuffle=True,
     )
+    return train_loader, test_loader
+
+
+
+
+if __name__ == '__main__':
+
+    EPOCH = 2
+    BATCH_SIZE = 16
+    test_num_batch = 50  #TODO: only for print?
+
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    print(device)
+
+    # Vision Transformer
+    model = ViTForImageClassification.from_pretrained('google/vit-base-patch16-224')
+    print_n_train_params(model)
+
+    # change the last layer
+    model.classifier = nn.Linear(in_features=768, out_features=18, bias=True)
+    print_n_train_params(model)
+
+    # LensDatasets
+    dataset_folder = Path("C:/Users/abcd2/Downloads/dev_256/")
+
+    train_loader, test_loader = get_train_test_dataloaders(dataset_folder, BATCH_SIZE)
 
 
     # model setup
     net = model
-    net.cuda()  #TODO: combine with net = model above?
+    net.cuda() 
     
     loss_fn = nn.MSELoss(reduction='sum')
-    optimizer = optim.Adam(net.parameters(), lr = 1e-4)
+    optimizer = optim.Adam(net.parameters(), lr=1e-4)
     tb = SummaryWriter()
 
-    best_accuracy = float("inf")
+    best_test_accuracy = float("inf")
 
     if not os.path.exists('./saved_model/'):
         os.mkdir('./saved_model/')
-        # net = torch.load('./saved_model/resnet18.mdl')
-        # print('loaded mdl!')
+    
+    #TODO: think about this. disconnected?
+    # net = torch.load('./saved_model/resnet18.mdl')  
+    # print('loaded mdl!')
 
     # training
     for epoch in range(EPOCH):
 
         net.train()
 
-        total_loss = 0.0
-        total_counter = 0
-        total_rms = 0
+        cache_train = initialize_cache()
 
         # for batch_idx, (data, target_dict) in enumerate(tqdm(train_loader, total=len(train_loader))):
         for batch_idx, (data, target_dict) in enumerate(train_loader):
             data, target = prepare_data_and_target(data, target_dict)
-
             optimizer.zero_grad()
-
-            output = net(data)[0]  #TODO: this is hard coded
-
-            loss_theta_E = loss_fn(100*output[0], 100*target[0])  #TODO: this is hard coded
-            loss_others = loss_fn(output, target)
-            loss = loss_theta_E + loss_others
-
-            square_diff = (output - target)
-            total_rms += square_diff.std(dim=0)
-            total_loss += loss.item()
-            total_counter += 1
-
+            output = net(data)[0] 
+            loss = calc_loss(loss_fn, output, target)
+            cache_train = update_cache(cache_train, output, target, loss)
             loss.backward()
             optimizer.step()
 
-        avg_rms = calc_avg_rms(total_rms, total_counter)
-        add_scalar_tb(avg_rms)
-
-        print(epoch, 'Train loss (averge per batch wise):', total_loss/(total_counter), 
-              ' RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
+        cache_train = calc_avg_rms(cache_train)
+        print_loss_rms(epoch, 'Train', cache_train)
 
 
         with torch.no_grad():
+
             net.eval()
 
-            total_loss = 0.0
-            total_counter = 0
-            total_rms = 0
+            cache_test = initialize_cache()
 
             for batch_idx, (data, target_dict) in enumerate(test_loader):
                 data, target = prepare_data_and_target(data, target_dict)
-
-                pred = net(data)[0]  #TODO: this is hard coded
-
-                loss_theta_E = loss_fn(100* pred[0], 100* target[0])  #TODO: this is hard coded
-                loss_others = loss_fn(pred, target)
-                loss = loss_theta_E + loss_others
-
-                square_diff = (pred - target)
-                total_rms += square_diff.std(dim=0)
-                total_loss += loss.item()
-                total_counter += 1
+                pred = net(data)[0]
+                loss = calc_loss(loss_fn, pred, target)
+                cache_test = update_cache(cache_test, pred, target, loss)
 
                 if batch_idx % test_num_batch == 0 and batch_idx != 0:
                     tb.add_scalar('test_loss', loss.item())
                     break
 
-            avg_rms = calc_avg_rms(total_rms, total_counter)
-            add_scalar_tb(avg_rms)
+            cache_test = calc_avg_rms(cache_test)
+            print_loss_rms(epoch, 'Test', cache_test)
 
-            print(epoch, 'Test loss (averge per batch wise):', total_loss/(total_counter), 
-                  ' RMS (average per batch wise):', np.array_str(avg_rms, precision=3))
-
-            #TODO: work on the saving part later
-            # if total_loss/(total_counter) < best_accuracy:
-            #     best_accuracy = total_loss/(total_counter)
-            #     datetime_today = str(datetime.date.today())
-            #     torch.save(net, './saved_model/' + datetime_today + 'power_law_pred_vit.mdl')
-            #     print("saved to " + "power_law_pred_vit.mdl" + " file.")
+            test_loss_per_batch = cache_test['total_loss'] / cache_test['total_counter']
+            if test_loss_per_batch < best_test_accuracy:
+                best_test_accuracy = test_loss_per_batch
+                datetime_today = str(datetime.date.today())
+                model_save_path = f'./saved_model/power_law_pred_vit_{datetime_today}.mdl'
+                torch.save(net, model_save_path)
+                print(f"save model to {model_save_path}")
 
     tb.close()
