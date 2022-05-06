6416c1879df6caf84b7ea869f86e93784231311d
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2022-05-06 17:38:59 -0400

    update saved model

--- model_vit.py
+++ model_vit.py
@@ -81,45 +81,6 @@ class DeepLenstronomyDataset(Dataset):
         return self.df.shape[0]
 
 
-def print_n_train_params(model):
-    """ Print number of trainable parameters.
-
-    Args:
-        model (model object): presumably a ViT model
-    """
-    n = sum(param.numel() for param in model.parameters() if param.requires_grad)
-    print(f"Number of trainable parameters: {n}")
-
-
-def prepare_data_and_target(data, target_dict, device):
-    """ Prepare data (X) and target (Y) for a given batch.
-
-    Args:
-        data (np.array): image (X)
-        target_dict (dict): targets (Y)
-        device (torch.device): cpu or gpu
-
-    Returns:
-        data (torch.Tensor): image (X)
-        target (torch.Tensor): targets(Y)
-    """
-    data = Variable(data.float()).to(device)
-    for key, val in target_dict.items():
-        target_dict[key] = Variable(val.float()).to(device)
-    target = torch.cat([val for _, val in target_dict.items()], dim=1)
-    return data, target
-
-
-
-
-class CacheHistory:
-
-    def __init__(self, cache_epoch):
-        self.cache_epoch = cache_epoch
-
-
-
-
 class CacheEpoch:
 
     def __init__(self):
@@ -170,6 +131,70 @@ class CacheEpoch:
         print(f"    loss (average over epoch) = {self.avg_loss:.4f}")
         print(f"    err_mean (average over epoch) = {avg_err_mean_print}")
         print(f"    err_std (average over epoch) = {avg_err_std_print}")
+
+
+class CacheHistory:
+
+    def __init__(self, tag):
+        """ Inintialize the loss history dict.
+
+        Returns:
+            (dict): empty loss history dict
+        """
+        self.tag = tag
+        self.history = {
+            "epoch": [],
+            "loss": [],
+        }
+
+    def record_and_save(self, epoch, loss, CONFIG):
+        """ Record loss to the history dict.
+
+        Args:
+            history_dict (dict): dict contains loss info
+            epoch (int): current epoch
+            batch_idx (int): current batch id 
+            loss (): current loss
+
+        Returns:
+            history_dict (dict): the updated history dict
+        """
+        self.history["epoch"].append(epoch)
+        self.history["loss"].append(loss)
+
+        fname = f"{CONFIG['dir_model_save']}/{CONFIG['model_file_name_prefix']}_{self.tag}_history.npy"
+        np.save(fname, self.history)
+
+
+
+
+def print_n_train_params(model):
+    """ Print number of trainable parameters.
+
+    Args:
+        model (model object): presumably a ViT model
+    """
+    n = sum(param.numel() for param in model.parameters() if param.requires_grad)
+    print(f"Number of trainable parameters: {n}")
+
+
+def prepare_data_and_target(data, target_dict, device):
+    """ Prepare data (X) and target (Y) for a given batch.
+
+    Args:
+        data (np.array): image (X)
+        target_dict (dict): targets (Y)
+        device (torch.device): cpu or gpu
+
+    Returns:
+        data (torch.Tensor): image (X)
+        target (torch.Tensor): targets(Y)
+    """
+    data = Variable(data.float()).to(device)
+    for key, val in target_dict.items():
+        target_dict[key] = Variable(val.float()).to(device)
+    target = torch.cat([val for _, val in target_dict.items()], dim=1)
+    return data, target
 
 
 def calc_loss(pred, target, CONFIG, device):
@@ -284,56 +309,17 @@ def prepare_vit_model(CONFIG):
     return model
 
 
-def initialize_loss_history():
-    """ Inintialize the loss history dict.
-
-    Returns:
-        (dict): empty loss history dict
-    """
-    return {
-        'epoch': [],
-        'batch_idx': [],
-        'loss': [],
-    }
-
-
-def record_loss_history(history_dict, epoch, batch_idx, loss):
-    """ Record loss to the history dict.
-
-    Args:
-        history_dict (dict): dict contains loss info
-        epoch (int): current epoch
-        batch_idx (int): current batch id 
-        loss (): current loss
-
-    Returns:
-        history_dict (dict): the updated history dict
-    """
-    history_dict['epoch'].append(epoch)
-    history_dict['batch_idx'].append(batch_idx)
-    history_dict['loss'].append(loss.item())
-    return history_dict
-
-
-def save_loss_history(CONFIG, history_dict, which):
-    """ Save loss history as npy.
-
-    Args:
-        CONFIG (dict): CONFIG
-        history_dict (dict): dict contains loss info
-        which (str): which loss history to be saved. 'train' or 'test'
-    """
-    fname = f"{CONFIG['dir_model_save']}/{CONFIG['model_file_name_prefix']}_{which}_loss_history.npy"
-    np.save(fname, history_dict)
-
-    # TODO: class
-    # dd = np.load(fname, allow_pickle=True)
-    # print(dd)
-
-
 def save_config(CONFIG):
     fname = f"{CONFIG['dir_model_save']}/{CONFIG['model_file_name_prefix']}_CONFIG.npy"
     np.save(fname, CONFIG)
+
+
+def save_model(CONFIG, model, epoch, test_loss):
+    _dir = CONFIG['dir_model_save']
+    _prefix = CONFIG['model_file_name_prefix']
+    model_save_path = f"{_dir}/{_prefix}_epoch_{epoch}_testloss_{test_loss:.6f}.mdl"
+    torch.save(model, model_save_path)
+    print(f"\nSave model to {model_save_path}\n")
 
 
 def train_model(CONFIG):
@@ -367,15 +353,15 @@ def train_model(CONFIG):
 
     optimizer = optim.Adam(model.parameters(), lr=CONFIG['init_learning_rate'])
 
-    best_test_accuracy = float("inf")  # to identify best model ever seen
-    test_loss_history = initialize_loss_history()
-    train_loss_history = initialize_loss_history()
-
+    best_test_loss = float("inf")  # to identify best model ever seen
+    
+    train_history = CacheHistory("train")
+    test_history = CacheHistory("test")
 
 
     for epoch in range(CONFIG['epoch']):
-        model.train()
 
+        model.train()
         cache_train = CacheEpoch()
 
         for batch_idx, (data, target_dict) in enumerate(tqdm(train_loader, total=len(train_loader))):
@@ -388,10 +374,6 @@ def train_model(CONFIG):
 
             loss.backward()
             optimizer.step()
-
-            if batch_idx % CONFIG['record_loss_every_num_batch'] == 0 and batch_idx != 0:
-                train_loss_history = record_loss_history(train_loss_history, epoch, batch_idx, loss)
-
 
         cache_train.calc_avg_across_batches()
         cache_train.print_loss_rms(epoch, 'Train')
@@ -408,25 +390,16 @@ def train_model(CONFIG):
 
                 cache_test.update_cache(pred, target, loss)
 
-                if batch_idx % CONFIG['record_loss_every_num_batch'] == 0 and batch_idx != 0:
-                    test_loss_history = record_loss_history(test_loss_history, epoch, batch_idx, loss)
-
             cache_test.calc_avg_across_batches()
             cache_test.print_loss_rms(epoch, 'Test')
 
-
-            # TODO: get test from history instead of cache
-            test_loss_per_batch = cache_test.avg_loss
-            if test_loss_per_batch < best_test_accuracy:
-                best_test_accuracy = test_loss_per_batch
-                _dir = CONFIG['dir_model_save']
-                _prefix = CONFIG['model_file_name_prefix']
-                model_save_path = f"{_dir}/{_prefix}_epoch_{epoch}_testloss_{test_loss_per_batch:.6f}.mdl"
-                torch.save(model, model_save_path)
-                print(f"\nSave model to {model_save_path}\n")
+            # save model with best test loss so far
+            if cache_test.avg_loss < best_test_loss:
+                best_test_loss = cache_test.avg_loss
+                save_model(CONFIG, model, epoch, cache_test.avg_loss)
     
-        for which, history_dict in zip(["train", "test"], [train_loss_history, test_loss_history]):
-            save_loss_history(CONFIG, history_dict, which)
+            train_history.record_and_save(epoch, cache_train.avg_loss, CONFIG)
+            test_history.record_and_save(epoch, cache_test.avg_loss, CONFIG)
 
 
 
@@ -443,7 +416,6 @@ if __name__ == '__main__':
         'dir_model_save': Path("C:/Users/abcd2/Downloads/tmp_dev_outputs"),
         'model_file_name_prefix': 'vit_dev',
         'init_learning_rate': 1e-4,
-        'record_loss_every_num_batch': 2,
         'target_keys_weights': {
             "theta_E": 10, 
             "gamma": 1, 
