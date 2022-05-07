c6e900a63976a96eefcb6b5bcecc1b3b51990f28
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2022-05-06 23:58:46 -0400

    update docsting

--- model_vit.py
+++ model_vit.py
@@ -81,13 +81,16 @@ class DeepLenstronomyDataset(Dataset):
 
 
 class CacheEpoch:
+    """ Cache within an epoch.
+    """
+    def __init__(self, tag):
+        """ Initialize cache attributes.
 
-    def __init__(self):
-        """ Initialize cache dict.
-
-        Returns:
-            cache (dict): initialized cache dict
+        Args:
+            tag (str): "train" or "test"
         """
+        self.tag = tag
+
         self._cnt = 0
         self._cum_loss = 0.0
         self._cum_err_mean = 0
@@ -98,12 +101,13 @@ class CacheEpoch:
         self.avg_err_std = 0
 
     def update_cache(self, pred, target, loss):
-        """ Update cache dict.
+        """ Update cache for a learning update of a batch to the
+            cummulative attributes.
 
         Args:
-            pred ([type]): [description]
-            target ([type]): [description]
-            loss ([type]): [description]
+            pred (torch.Tensor): predictions of the batch samples
+            target (torch.Tensor): targets of the batch samples
+            loss (torch.Tensor): weighted averaged loss of the batch
         """
         err = (pred - target).cpu().detach().numpy()
         self._cum_err_mean += err.mean(axis=0)
@@ -112,33 +116,35 @@ class CacheEpoch:
         self._cnt += 1
 
     def calc_avg_across_batches(self):
+        """ Calculate the average of every attribute within a complete
+            epoch (accross all batches).
+        """
         self.avg_err_mean = self._cum_err_mean / self._cnt
         self.avg_err_std = self._cum_err_std / self._cnt
         self.avg_loss = self._cum_loss / self._cnt
 
-    def print_loss_rms(self, epoch, train_test_str):
-        """ Print loss and rms.
+    def print_cache(self, epoch):
+        """ Print cache attributes (average over epoch).
 
         Args:
-            epoch ([type]): [description]
-            train_test_str ([type]): [description]
-            cache ([type]): [description]
+            epoch (int): epoch
         """
         avg_err_mean_print = np.array_str(self.avg_err_mean, precision=4)
         avg_err_std_print = np.array_str(self.avg_err_std, precision=4)
-        print(f"\nepoch = {epoch}, {train_test_str}:")
-        print(f"    loss (average over epoch) = {self.avg_loss:.4f}")
-        print(f"    err_mean (average over epoch) = {avg_err_mean_print}")
-        print(f"    err_std (average over epoch) = {avg_err_std_print}")
+        print(f"\nepoch = {epoch}, {self.tag}:")
+        print(f"    loss = {self.avg_loss:.4f}")
+        print(f"    err_mean = {avg_err_mean_print}")
+        print(f"    err_std = {avg_err_std_print}")
 
 
 class CacheHistory:
-
+    """ Cache history for all epochs.
+    """
     def __init__(self, tag):
-        """ Inintialize the loss history dict.
+        """ Inintialize cache history (dict).
 
-        Returns:
-            (dict): empty loss history dict
+        Args:
+            tag (str): "train" or "test"
         """
         self.tag = tag
         self.history = {
@@ -149,16 +155,12 @@ class CacheHistory:
         }
 
     def record_and_save(self, epoch, cache_epoch, CONFIG):
-        """ Record loss to the history dict.
+        """ Record all caches within a single epoch. Save history dict.
 
         Args:
-            history_dict (dict): dict contains loss info
             epoch (int): current epoch
-            batch_idx (int): current batch id 
-            loss (): current loss
-
-        Returns:
-            history_dict (dict): the updated history dict
+            cache_epoch (CacheEpoch): cache_epoch object 
+            CONFIG (dict): CONFIG
         """
         self.history["epoch"].append(epoch)
         self.history["loss"].append(cache_epoch.avg_loss)
@@ -370,7 +372,7 @@ def train_model(CONFIG):
     for epoch in range(CONFIG['epoch']):
 
         model.train()
-        cache_train = CacheEpoch()
+        cache_train = CacheEpoch("train")
 
         for data, target_dict in tqdm(train_loader, total=len(train_loader)):
             data, target = prepare_data_and_target(data, target_dict, device)
@@ -384,12 +386,12 @@ def train_model(CONFIG):
             optimizer.step()
 
         cache_train.calc_avg_across_batches()
-        cache_train.print_loss_rms(epoch, 'Train')
+        cache_train.print_cache(epoch)
 
 
         with torch.no_grad():
             model.eval()
-            cache_test = CacheEpoch()
+            cache_test = CacheEpoch("test")
 
             for data, target_dict in test_loader:
                 data, target = prepare_data_and_target(data, target_dict, device)
@@ -399,7 +401,7 @@ def train_model(CONFIG):
                 cache_test.update_cache(pred, target, loss)
 
             cache_test.calc_avg_across_batches()
-            cache_test.print_loss_rms(epoch, 'Test')
+            cache_test.print_cache(epoch)
 
             # save model with best test loss so far
             if cache_test.avg_loss < best_test_loss:
