40c18fefa78083cf225ae36f50d3c3c4f4a05079
Kuan-Wei Huang <22647448+kuanweih@users.noreply.github.com>
2022-07-17 22:11:51 -0400

    add BayesianInference

--- predict.py
+++ predict.py
@@ -1,6 +1,7 @@
 """ Module to made prediction for test set from a given model.
 """
 
+import glob
 import torch 
 import numpy as np
 import pandas as pd
@@ -14,12 +15,17 @@ from src.data_utils import get_test_dataloader, get_test_dataset
 
 
 class ModelPredictor:
-    def __init__(self, dir_output, path_model, leave_dropout_on=False,
+    def __init__(self, dir_output, path_model, leave_dropout_on=False, dir_pred=None,
                  dataset_folder=Path("C:/Users/abcd2/Datasets/2022_icml_lens_sim/geoff_1200")):
         self.dir_output = dir_output                 
         self.path_model = path_model
         self.leave_dropout_on = leave_dropout_on
         self.dataset_folder = dataset_folder
+
+        if dir_pred is not None:
+            self.dir_pred = dir_pred
+        else:
+            self.dir_pred = self.dir_output
 
         path_config = Path(f"{dir_output}/CONFIG.npy")
         self.CONFIG = np.load(path_config, allow_pickle=True).item()
@@ -35,12 +41,14 @@ class ModelPredictor:
 
         self.model = torch.load(path_model)
         self.model.to(self.device)
-        if not leave_dropout_on:
+        if leave_dropout_on:
+            self.model.train()
+        else:
             self.model.eval()
         
         self.targets_list = self.CONFIG["target_keys_weights"].keys()
 
-    def execute(self):
+    def execute(self, saved_file_suffix=None):
         print("Start predicting\n")
 
         pred_dict = {k: [] for k in self.targets_list}
@@ -74,14 +82,18 @@ class ModelPredictor:
 
         self.df_result = pd.concat([df_truth, df_pred, df_sigma], axis=1)
 
-        if self.leave_dropout_on:
-            path_pred_scaled = Path(f"{self.dir_output}/pred_scaled_dp.csv")
-        else:
-            path_pred_scaled = Path(f"{self.dir_output}/pred_scaled.csv")
-        self.df_result.to_csv(path_pred_scaled, index=False)
-        print(f"Saved pred_scaled.csv to {path_pred_scaled} \n")
+        # # not neccessary
+        # if self.leave_dropout_on:
+        #     if saved_file_suffix is not None:
+        #         path_pred_scaled = Path(f"{self.dir_pred}/pred_scaled_dp_{saved_file_suffix}.csv")
+        #     else:
+        #         path_pred_scaled = Path(f"{self.dir_pred}/pred_scaled_dp.csv")
+        # else:
+        #     path_pred_scaled = Path(f"{self.dir_pred}/pred_scaled.csv")
+        # self.df_result.to_csv(path_pred_scaled, index=False)
+        # print(f"Saved pred_scaled.csv to {path_pred_scaled} \n")
 
-    def scale_back(self, path_scaler=Path("C:/Users/abcd2/Datasets/2022_icml_lens_sim/geoff_30000/scaler.pkl")):
+    def scale_back(self, saved_file_suffix=None, path_scaler=Path("C:/Users/abcd2/Datasets/2022_icml_lens_sim/geoff_30000/scaler.pkl")):
         print("Start scaling pred_scaled.csv back\n")
 
         scaler = load(open(path_scaler, 'rb'))
@@ -103,9 +115,12 @@ class ModelPredictor:
         self.df_resumed = pd.DataFrame(df_resumed)
 
         if self.leave_dropout_on:
-            path_pred = Path(f"{self.dir_output}/pred_dp.csv")
+            if saved_file_suffix is not None:
+                path_pred = Path(f"{self.dir_pred}/pred_dp_{saved_file_suffix}.csv")
+            else:
+                path_pred = Path(f"{self.dir_pred}/pred_dp.csv")
         else:
-            path_pred = Path(f"{self.dir_output}/pred.csv")
+            path_pred = Path(f"{self.dir_pred}/pred.csv")
         self.df_resumed.to_csv(path_pred, index=False)
         print(f"Scaled back and saved pred.csv to {path_pred} \n")
 
@@ -114,11 +129,51 @@ class ModelPredictor:
         # for target in self.targets_list:
         #     key = f"{target}____truth"
         #     print(np.mean((self.df_resumed[key] - df_meta[target])**2))
+
+
+
+
+class BayesianInference:
+    def __init__(self, dir_pred, dir_output):
+        self.dir_pred = dir_pred
+        self.dir_output = dir_output
+
+        self.file_paths = [path for path in self.dir_pred.glob('**/*') if path.is_file()]
         
+        self.targets, self.res_dict = self._get_targets_and_init_dict(self.file_paths[0])
+        self.posterior_dict = self._calc_posteriors()
+        
+        self.res_dict = {**self.res_dict, **self.posterior_dict}
+        np.save(f"{self.dir_output}/posterior.npy", self.res_dict)
 
-                
-                
-                
+    def _get_targets_and_init_dict(self, file_path):
+        res_dict = {}
+        targets = []
+        df = pd.read_csv(file_path)
+        all_keys = list(df.keys())
+        for key in all_keys:
+            if key.endswith("____truth"):
+                res_dict[key] = df[key].values
+                target = key.replace("____truth", "")
+                targets.append(target)
+        return targets, res_dict
+    
+    def _calc_posteriors_single_file(self, file_path, posterior_dict):
+        df = pd.read_csv(file_path)
+        for target in self.targets:
+            pred = df[f"{target}____pred"]
+            sigma = df[f"{target}____sigma"]
+            posterior = np.random.normal(loc=pred, scale=sigma)
+            posterior_dict[target].append(posterior)
+        return posterior_dict
 
-
-
+    def _calc_posteriors(self):
+        posterior_dict = {target: [] for target in self.targets}
+        for file_path in tqdm(self.file_paths):
+            posterior_dict = self._calc_posteriors_single_file(file_path, posterior_dict)
+        
+        keys = [key for key in posterior_dict.keys()]
+        for key in keys:
+            posterior_dict[key] = np.array(posterior_dict[key])
+            posterior_dict[f"{key}____posterior"] = posterior_dict.pop(key)
+        return posterior_dict
